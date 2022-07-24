aa08693c12507bbb55dcf3bd00b4cea32ed3e41e
MoonSanghee <psycho13th@gmail.com>
2022-07-24 14:03:00 +0900

    백준 8-4까지 완

--- Baekjoon/8/4.py
+++ Baekjoon/8/4.py
@@ -2,10 +2,13 @@
 
 m, n = map(int, input().split())
 for i in range(m, n + 1):
-    check_list = []
-    for j in range(2, i):
+    if i == 1:
+    #1은 소수가 아니므로 제외
+        continue
+    for j in range(2, int(i ** 0.5) + 1):
+        # i의 제곱근까지 인수를 가지지 못하면 소수 일 수밖에 없다.
+        # 범위는 정수여야 하므로 i ** 0.5를 정수형으로 출력시킨다.
         if i % j == 0:
-            check_list.append(j)
-    if len(check_list) == 2:
-        print(check_list[1])
-
+            break
+    else:
+        print(i)
