a49d31196053856eff5d556281f883bf6376a7b7
MoonSanghee <psycho13th@gmail.com>
2022-07-31 14:40:20 +0900

    SWEA D2 1961문제 풀이 완

--- SWEA/D2/1961.py
+++ SWEA/D2/1961.py
@@ -1,8 +1,26 @@
-t = int(input())
-for i in range(t):
+import sys
+
+sys.stdin = open("1961_input.txt", "r")
+
+test_case = int(input())
+
+for test_num in range(1, test_case + 1):
+    print(f'#{test_num}')
+    
     n = int(input())
-    a = []
-    for j in range(n):
-        b = list(map(int, input().split()))
-        a.append(b)
-    print(a)
+    seet = []
+    for i in range(n):
+        line = list(map(int, input().split()))
+        seet.append(line)
+    
+
+    for i in range(n):
+        turn_90, turn_180, turn_270 = '', '', ''
+        for j in range(n):
+            turn_90 += str(seet[n - 1 - j][i])
+            turn_180 += str(seet[n - 1 - i][n -1 - j])
+            turn_270 += str(seet[j][n - 1 - i])
+
+
+        
+        print(turn_90, turn_180, turn_270)
