ec3e55b1f7407f5fcf7e1669f25709a66f506af6
MoonSanghee <psycho13th@gmail.com>
2022-08-19 17:14:02 +0900

    실습 확인하며 수정

--- Database/db_01/04실습.sql
+++ Database/db_01/04실습.sql
@@ -34,7 +34,7 @@ FROM customers;
 SELECT FirstName "이름", LastName "성"
 FROM customers
 WHERE Country = 'USA'
-ORDER BY "이름"
+ORDER BY 이름 DESC
 LIMIT 5;
 
 6. 송장(invoices) 테이블에서 `BillingPostalCode`가 `NULL` 이 아닌 행의 개수를 출력하세요.
@@ -45,10 +45,10 @@ WHERE BillingPostalCode IS NOT NULL;
 
 7. 송장(invoices) 테이블에서 `BillingState`가 `NULL` 인 데이터를 출력하세요.
 | 단, `InvoiceDate`를 기준으로 내림차순으로 5개까지 출력하세요.
-SELECT *
+SELECT * 
 FROM invoices
 WHERE BillingPostalCode IS NULL
-ORDER BY InvoiceDate
+ORDER BY InvoiceDate DESC
 LIMIT 5;
 
 8. 송장(invoices) 테이블에서 `InvoiceDate`의 년도가 `2013`인 행의 개수를 출력하세요.
@@ -59,13 +59,13 @@ WHERE strftime('%Y', InvoiceDate) = '2013';
 
 9. 고객(customers) 테이블에서 `FirstName`이 `L` 로 시작하는 고객의 `CustomerId`, `FirstName`, `LastName`을 출력하세요.
 | 단, 각각의 컬럼명을 `고객ID`, `이름`,`성`으로 출력하고, `고객ID`을 기준으로 오름차순으로 출력하세요.
-SELECT CustomerId, FirstName, LastName
+SELECT CustomerId "고객ID", FirstName "이름", LastName "성"
 FROM customers
 WHERE FirstName LIKE 'L%'
-ORDER BY CustomerId;
+ORDER BY 고객ID;
 
 10. 고객(customers) 테이블에서 각 나라의 고객 수와 해당 나라 이름을 출력하세요.
-SELECT country "나라", COUNT(*) "고객 수"
+SELECT COUNT(*) "고객 수", country "나라"
 FROM customers
 GROUP BY country
 ORDER BY COUNT(*) DESC
@@ -89,11 +89,11 @@ ORDER BY COUNT(*) DESC;
 13. 고객(customers) 테이블에서 `State`가 존재하는 고객들을 `Country` 와 `State`를 기준으로 그룹화해서 각 그룹의 `고객 수`, `Country`, `State` 를 출력하세요.
 | 단, `고객 수`, `Country` 순서 기준으로 내림차순으로 5개까지 출력하세요.
 
-SELECT COUNT(*) "고객수", Country, State
+SELECT COUNT(*) "고객 수", Country, State
 FROM customers
-WHERE State IS NOT NULL
+where State IS NOT NULL
 GROUP BY Country, State
-ORDER BY COUNT(*) DESC
+ORDER BY "고객 수" DESC, Country DESC
 LIMIT 5;
 
 14.  고객(customers) 테이블에서 `Fax` 가 `NULL`인 고객은 'X' NULL이 아닌 고객은 'O'로 `Fax 유/무` 컬럼에 표시하여 출력하세요.
@@ -102,7 +102,8 @@ SELECT CustomerId,
         WHEN Fax IS NULL THEN 'X'
         WHEN Fax IS NOT NULL THEN 'O'
     END "Fax 유/무"
-FROM customers;
+FROM customers
+LIMIT 5;
 
 15. 점원(employees) 테이블에서 `올해년도 - BirthDate 년도 + 1` 를 계산해서 `나이` 컬럼에 표시하여 출력하세요.
 SELECT LastName, FirstName, (strftime('%Y', 'now') - strftime('%Y', BirthDate) + 1) "나이"
