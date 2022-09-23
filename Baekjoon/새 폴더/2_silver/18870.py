b147ec30a923d9c257f03728344732ad3972c9d8
MoonSanghee <psycho13th@gmail.com>
2022-09-24 00:35:29 +0900

    백준 18870 문제 풀이 추가

---
+++ Baekjoon/새 폴더/2_silver/18870.py
@@ -0,0 +1,10 @@
+n = int(input())
+li = list(map(int, input().split()))
+new = sorted(list(set(li)))
+di = dict()
+for i in range(len(new)):
+    if new[i] not in di:
+        di[new[i]] = i
+for i in range(n):
+    li[i] = di[li[i]]
+print(*li)
--- Baekjoon/새 폴더/2_silver/test.py
+++ Baekjoon/새 폴더/2_silver/test.py
@@ -1,11 +1,1 @@
-# 18870
-# n = int(input())
-# li = list(map(int, input().split()))
-# new = sorted(list(set(li)))
-# di = dict()
-# for i in range(len(new)):
-#     if new[i] not in di:
-#         di[new[i]] = i
-# for i in range(n):
-#     li[i] = di[li[i]]
-# print(li)
+
