a9dafa63f57de0e62d759d3ec6dd70103e737270
MoonSanghee <psycho13th@gmail.com>
2022-09-29 23:50:48 +0900

    백준 11728 투포인트 풀이 추가

--- Baekjoon/새 폴더/2_silver/11728.py
+++ Baekjoon/새 폴더/2_silver/11728.py
@@ -1,7 +1,33 @@
-n, m = map(int,input().split())
+# n, m = map(int,input().split())
+# a = list(map(int, input().split()))
+# b = list(map(int, input().split()))
+# for i in range(m):
+#     a.append(b[i])
+# a.sort()
+# print(*a)
+
+# 투 포인트 이용한 풀이
+n,m = map(int, input().split())
 a = list(map(int, input().split()))
 b = list(map(int, input().split()))
-for i in range(m):
-    a.append(b[i])
 a.sort()
-print(*a)
+b.sort()
+s1 = 0
+s2 = 0
+c = []
+while len(c) < n + m:
+    i = a[s1]
+    j = b[s2]
+    if i > j:
+        c.append(j)
+        s2 += 1
+        if s2 == m:
+            b[-1] = 1000000001
+            s2 -= 1
+    else:
+        c.append(i)
+        s1 += 1
+        if s1 == n:
+            a[-1] = 1000000001
+            s1 -= 1
+print(*c)
