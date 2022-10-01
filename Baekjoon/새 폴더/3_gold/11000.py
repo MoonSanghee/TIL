09c6b53763f759045b4351cce1ac08fd35376151
MoonSanghee <psycho13th@gmail.com>
2022-10-02 02:00:36 +0900

    백준 11000 문제 풀이 추가

--- Baekjoon/새 폴더/3_gold/11000.py
+++ Baekjoon/새 폴더/3_gold/11000.py
@@ -1,4 +1,5 @@
-import heapq
+import heapq, sys
+input = sys.stdin.readline
 n = int(input())
 li = []
 for i in range(n):
@@ -8,7 +9,8 @@ rooms = [li[0][1]]
 for i in range(1, n):
     if rooms[0] > li[i][0]:
         heapq.heappush(rooms, li[i][1])
+
     else:
         heapq.heappop(rooms)
-        heapq.heappush(rooms, li[i][1])
+
 print(len(rooms))
