387ecddebaac1a02ebe01ae53bd0621d60ba7802
MoonSanghee <psycho13th@gmail.com>
2022-10-05 20:48:00 +0900

    백준 2605 문제 풀이 추가

--- Baekjoon/새 폴더/1_Bronze/test.py
+++ Baekjoon/새 폴더/1_Bronze/test.py
@@ -1,3 +1,10 @@
-a = [1, 2, 3]
-a.remove(1)
-print(a)
+n = int(input())
+li = []
+for i in range(n):
+    li.append(i + 1)
+nums = list(map(int, input().split()))
+for i in range(n):
+    moved = i - nums[i]
+    for j in range(i, moved, -1):
+        li[j], li[j - 1] = li[j - 1], li[j]
+print(*li)
