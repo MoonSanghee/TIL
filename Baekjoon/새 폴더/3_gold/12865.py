f9a9157cb0e5693e5044929d380d2f87137fd878
MoonSanghee <psycho13th@gmail.com>
2022-10-06 10:04:43 +0900

    백준 12865 문제 풀이 추가

--- Baekjoon/새 폴더/3_gold/12865.py
+++ Baekjoon/새 폴더/3_gold/12865.py
@@ -6,5 +6,6 @@ for i in range(n):
 for item in items:
     w, v = item
     for i in range(k, w - 1, -1):
+        # 제한 무게는 k에서 아이템의 무게까지 역순으로 가장 가치있는 조합을 찾아야한다.
         dp[i] = max(dp[i], dp[i - w] + v)
 print(dp[-1])
