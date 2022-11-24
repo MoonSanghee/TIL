0f1224b9ba5d15b1e738a5f0b7e3b7c889ec4f93
MoonSanghee <psycho13th@gmail.com>
2022-11-24 21:38:01 +0900

    백준 13023 문제풀이 추가

---
+++ Baekjoon/새 폴더/3_gold/13023.py
@@ -0,0 +1,41 @@
+n, m = map(int, input().split())
+
+li = [[] for _ in range(n)]
+# 관계를 표시해줄 관계리스트입니다
+check = [False for _ in range(2001)]
+# 방문을 확인하는 리스트입니다.
+result = False
+# 깊이가 충분한지는 기본을 False로 설정합니다.
+
+for i in range(m):
+    a, b = map(int, input().split())
+    li[a].append(b)
+    li[b].append(a)
+    # 입력되는 관계에 따라 양방향 그래프를 표시해줍니다.
+
+def dfs(x, y):
+    global result
+    check[x] = True
+    if y == 4:
+        result = True
+        return 
+    for i in li[x]:
+        if not check[i]:
+            check[i] = True
+            dfs(i, y + 1)
+            check[i] = False
+# 방문을 확인하는 dfs를 만들고 깊이를 확인할 변수를 같이 입력하여 깊이가 충분하면 
+# result 값을 True로 변환하여 출력해줍니다.
+for i in range(n):
+    # 모든 변수에서 출발하여 
+    dfs(i, 0)
+    check[i] = False
+    if result:
+        break
+    # result 값이 True로 반환되었다면 조건의 관계를 충족하므로 반복을 그만해도 됩니다.
+if result:
+    print(1)
+    # result값이 True라면 1을
+else:
+    print(0)
+    # 아니면 0을 출력합니다.
