39b2a2f0c8640f2388322a99dc7f26b1aa2f8205
MoonSanghee <psycho13th@gmail.com>
2023-01-30 20:05:03 +0900

    백준 11725 문제풀이 추가

--- Baekjoon/새 폴더/2_silver/11725.py
+++ Baekjoon/새 폴더/2_silver/11725.py
@@ -1,7 +1,27 @@
+from collections import deque
 n = int(input())
-maps = [[]] + [[]] * n
-check = [0] * (n + 1)
+graph = [[] for _ in range(n + 1)]
+# 노드간의 연결 그래프를 표시해줄 리스트를 만들어 줍니다/
+visited = [False] * (n + 1)
+# 방문처리를 해줄 리스트를 만들어 줍니다.
 for i in range(n - 1):
     a, b = map(int, input().split())
-    maps[a].append(b)
-    
+    graph[a].append(b)
+    graph[b].append(a)
+
+visited[1] = True
+# n - 1 번만큼 관계를 받아주고 1번 노드를 방문처리 하여줍니다.
+q = deque()
+q.append(1)
+while q:
+    x = q.popleft()
+    for i in graph[x]:
+        if visited[i] == False:
+            visited[i] = x
+            q.append(i)
+    # q라는 이름의 덱을 만들고 1을 넣어줍니다. q의 가장 앞의 값을 뽑아 연결된 
+    # 노드들을 방문처리 해주고 부모 노드를 기록해 줍니다. 
+    # 모든 연결된 노드를 방문할 때 까지 시행하여 줍니다.
+for i in range(2, n + 1):
+    print(visited[i])
+    # 2번 노드부터 부모 노드를 차례로 출력하여 줍니다.
