bbecf8661ae7d0a131386c93f88c128bf38f0ea2
MoonSanghee <psycho13th@gmail.com>
2023-02-04 11:02:45 +0900

    백준 1261 문제풀이 주석 추가

--- Baekjoon/새 폴더/3_gold/1261_1.py
+++ Baekjoon/새 폴더/3_gold/1261_1.py
@@ -2,13 +2,14 @@ from collections import deque
 
 n, m = map(int, input().split())
 maps = [list(map(int, input())) for _ in range(m)]
-
+# 영역의 크기와 영역의 상태를 받아줍니다.
 visited = [[[0, 10000]]*n for _ in range(m)]
 visited[0][0] = [0, 0]
-
+# 최대 사이즈가 100*100이므로 비용이 0만큼 들고 10000번 벽을 부쉈다는 리스트로
+# 영역의 크기와 같은 방문확인할 리스트를 만들엊줍니다.
 dx = [1, -1, 0, 0]
 dy = [0, 0, 1, -1]
-
+# 4방위 탐색을 지정해줍니다.
 def move(a, b):
     q = deque()
     q.append((a, b))
@@ -19,21 +20,29 @@ def move(a, b):
             ny = y + dy[i]
             if nx in range(m) and ny in range(n):
                 if maps[nx][ny] == 0:
+                    # 영역에서 벽이 아니라면
                     if visited[nx][ny][1] > visited[x][y][1]:
                         visited[nx][ny] = [visited[x][y][0] + 1, visited[x][y][1]]
                         q.append((nx, ny))
+                        # 벽을 최소로 부수었는지 확인해주고 벽을 최소로 부수고
+                        # 도달하도록 갱신해준 다음 좌표를 넣어주고
                     elif visited[nx][ny][1] == visited[x][y][1]:
                         if visited[nx][ny][0] > visited[x][y][0] + 1:
                             visited[nx][ny][0] = visited[x][y][0] + 1
                             q.append((nx, ny))
+                        # 벽이라면 최단거리인지 확인해주고 최단거리로 갱신한 후 좌표를 넣어줍니다.
                 else:
                     if visited[nx][ny][1] > visited[x][y][1] + 1:
                         visited[nx][ny] = [visited[x][y][0] + 1, visited[x][y][1] + 1]
                         q.append((nx, ny))
+                        # 영역에서 벽이라면 벽을 1번 더 부수었을때 최소로 부수는 것인지 확인해주고
+                        # 값을 갱신해 넣어주고
                     elif visited[nx][ny][1] == visited[x][y][1] + 1:
                         if visited[nx][ny][0] > visited[x][y][0] + 1:
                             visited[nx][ny][0] = visited[x][y][0] + 1
                             q.append((nx, ny))
+                        # 최소로 부수는 것이라면 최단거리인지 확인해주고 좌표를 넣어줍니다.
 
 move(0, 0)
 print(visited[-1][-1][1])
+# 목적지에 저장된 최소로 부순 벽의 개수를 출력해줍니다.
--- Baekjoon/새 폴더/3_gold/1261_2.py
+++ Baekjoon/새 폴더/3_gold/1261_2.py
@@ -5,24 +5,29 @@ n, m = map(int, input().split())
 maps = [list(map(int, input())) for _ in range(m)]
 INF = int(1e9)
 visited = [[INF] * n for _ in range(m)]
-
+# 영역과 방문 확인을 표시할 리스트를 만들어줍니다,
 dx = [1, -1, 0, 0]
 dy = [0, 0, 1, -1]
-
+# 4방위 이동을 지정해줍니다.
 result = []
 heappush(result, (0, 0, 0))
 visited[0][0] = 0
-
+# 현재 위치까지 벽을 얼마나 부수었는지와 x, y좌표를 튜플로 묶어 heappush해줍니다.
 while result:
     cost, x, y = heappop(result)
     if x == m - 1 and y == n - 1:
         print(cost)
+        # x, y가 목적지라면 부순 벽의 횟수를 출력해줍니다.
         break
     for i in range(4):
         nx = dx[i] + x
         ny = dy[i] + y
         if nx in range(m) and ny in range(n):
             new_cost = cost + maps[nx][ny]
-            if new_cost + 1 < visited[nx][ny]:
+            # 현재까지 부순 벽의 수에 이동하는데 필요한 벽을 더하여 new_cost라 정하고
+            if new_cost < visited[nx][ny]:
                 visited[nx][ny] = new_cost
                 heappush(result, (new_cost, nx, ny))
+                # new_cost가 현재 저장된 부수어야하는 벽보다 적다면 값을 갱신하고
+                # 현 좌표에 도착하기위해 최소로 부수어야하는 벽의 수를 갱신해주고
+                # heappush로 부순 벽의 수와 좌표를 넣어줍니다. 
