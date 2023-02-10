7babc80005275afede9a0506cfd5390fd2eeb91b
MoonSanghee <psycho13th@gmail.com>
2023-02-10 13:58:07 +0900

    백준 1924 주석 추가

--- Baekjoon/새 폴더/1_Bronze/1924.py
+++ Baekjoon/새 폴더/1_Bronze/1924.py
@@ -1,8 +1,10 @@
+x, y = map(int, input().split())
 month = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-day = ['MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT', 'SUN']
-m, d = map(int, input().split())
-cnt = d - 1
-for i in range(m - 1):
-    cnt += month[i]
+# 몇월 몇일인지 받아주고
+for i in range(x - 1):
+    y += month[i]
+# 입력된 달의 전 달만큼의 일수를 받은 일에 더하고
+d = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT']
 
-print(day[cnt % 7])
+print(d[(y%7)])
+# 'SUN'-'SAT'까지 담긴 리스트에서 7로 나누어 남은 값의 인덱스자리 값을 출력해줍니다.
