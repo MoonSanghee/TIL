2c807eafd8f593949dbc4f7ff126ee5fc6b5a197
MoonSanghee <psycho13th@gmail.com>
2023-05-14 15:04:22 +0900

    백준 1735 문제풀이 추가

--- Baekjoon/새 폴더/2_silver/13414.py
+++ Baekjoon/새 폴더/2_silver/13414.py
@@ -2,17 +2,18 @@ import sys
 input = sys.stdin.readline
 
 k, l = map(int, input().split())
-
+# 두 수를 받아줍니다.
 wait = dict()
 
 for i in range(l):
     num = input().strip()
     wait[num] = i
-
+    # 각 수를 받아 새로 나왔다면 수서를 뒤에 나온 차례로 갱신해줍니다.
 result = sorted(wait.items(), key = lambda x:x[1])
-
+# 차례순으로 정렬하여줍니다.
 if (k > len(result)):
     k = len(result)
-
+# 원하던 길이가  충분히 길지 않다면 현재 딕셔너리의 길이만큼 출력한다고 갱신해줍니다.
 for i in range(k):
     print(result[i][0])
+# 출력해야하는 길이만큼만 출력해줍니다.
