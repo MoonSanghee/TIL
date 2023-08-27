eee2b879441ebf7650642eae26c12e137cb1470b
MoonSanghee <psycho13th@gmail.com>
2023-08-27 12:02:07 +0900

    백준 3062 문제풀이 추가

---
+++ Baekjoon/새 폴더/1_Bronze/3062.py
@@ -0,0 +1,14 @@
+n = int(input())
+# 수의 개수를 받아줍니다.
+for _ in range(n) :
+  number = input()
+#   수를 받아줍니다.
+  reverseNumber = number[::-1]
+#   뒤집은 수를 구해줍니다.
+  sumValue = str(int(n) + int(reverseNumber))
+#   합을 구해줍니다.
+  if sumValue == sumValue[::-1] :
+    print("YES")
+  else :
+    print("NO")
+    # 합한 값의 형태를 확인하고 결과를 출력해줍니다.
