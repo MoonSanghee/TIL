0cbfe8a68c214a814b69077aec441805d31aa871
MoonSanghee <psycho13th@gmail.com>
2023-09-06 11:32:27 +0900

    백준 10812 문제풀이 추가

---
+++ Baekjoon/새 폴더/1_Bronze/10812.py
@@ -0,0 +1,9 @@
+N, M = map(int, input().split())
+buckets = [i for i in range(1, N+1)]
+
+for _ in range(M):
+    i, j, k = map(int, input().split())
+    i, j, k = i-1, j-1, k-1
+    buckets = buckets[:i] + buckets[k:j+1] + buckets[i:k] + buckets[j+1:]
+    
+print(*buckets)
