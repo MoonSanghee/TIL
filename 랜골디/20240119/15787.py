ff78f27e9e8e5f23547c90cfa629262170afda4b
MoonSanghee <psycho13th@gmail.com>
2024-01-19 16:57:11 +0900

    백준 18234 문제풀이 추가

--- 랜골디/20240119/15787.py
+++ 랜골디/20240119/15787.py
@@ -1,5 +1,6 @@
 n, m = map(int, input().split())
 trains = [[False] * 20 for _ in range(n)]
+# 열차의 수와 명령을 받고 n개의 열차를 만들어 줍니다.
 for _ in range(m):
     command = list(map(int, input().split()))
     if command[0] == 1:
@@ -14,10 +15,11 @@ for _ in range(m):
         for i in range(19):
             trains[command[1] - 1][i] = trains[command[1] - 1][i + 1]
         trains[command[1] - 1][19] = False
-
+    # 명령어를 실행시켜줍니다.
 result = []
 for train in trains:
     if train not in result:
         result.append(train)
-
+# 빈 리스트에 같은 상태의 기차가 없는지 확인하여 처음 나온 상태의 기차만 추가해줍니다.
 print(len(result))
+# 다른 상태의 기차 수를 출력해줍니다.
