1e823c8c237ec87cfef0b9ac09021fbb02105513
MoonSanghee <psycho13th@gmail.com>
2024-02-01 17:10:59 +0900

    백준 14726 문제풀이 추가

--- Baekjoon/새 폴더/1_Bronze/4328.py
+++ Baekjoon/새 폴더/1_Bronze/4328.py
@@ -2,16 +2,18 @@ while True:
     li = list(map(int, input().split()))
     if li[0] == 0 and len(li) == 1:
         break
-    
+    # 수를 받아줍니다.
     b, p, m = li[0], li[1], li[2]
-
+    # b, p, m을 구해줍니다.
     n = int(str(p), b) % int(str(m), b)
+    # b진수 p,m을 10진수로 변환시켜 나눈값을 구해줍니다.
     result = []
     
     while n >= b:
         result.append(str(n % b))
         n = n // b
+        # 나머지 n을 b진법으로 변환해줍니다.
     
     result.append(str(n))
-
     print(int(''.join(result[::-1])))
+    # b진법으로 바꾼 결과를 출력해줍니다.
