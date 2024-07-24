2d943240c39be26cdb30f0e082bdaf6475a28c52
MoonSanghee <psycho13th@gmail.com>
2024-07-24 12:43:07 +0900

    백준 25024 문제 풀이 설명 추가

--- Baekjoon/새 폴더/1_Bronze/25024.py
+++ Baekjoon/새 폴더/1_Bronze/25024.py
@@ -1,13 +1,13 @@
 t = int(input())
-
+# 입력되는 테스트 케이스의 수를 받아줍니다.
 for _ in range(t):
     x, y = map(int, input().split())
-    
+    # x, y 값을 받아줍니다.
     if 0 <= x <= 23 and 0 <= y <= 59:
         time = 'Yes'
     else:
         time = 'No'
-    
+    # 시간으로 출력가능한지 확인하고 출력할 값을 변수에 담아줍니다.
     if 1 <= x <= 12:
         if x == 2 and 1 <= y <= 29:
             date = "Yes"
@@ -19,5 +19,6 @@ for _ in range(t):
             date = "No"
     else:
         date = "No"
-    
+    # 날짜로 출력 가능한지 확인하고 변수에 출력할 값을 담아줍니다.
     print(f'{time} {date}')
+    # 시간과 날짜로 출력 가능한지 형식에 맞게 출력해줍니다.
