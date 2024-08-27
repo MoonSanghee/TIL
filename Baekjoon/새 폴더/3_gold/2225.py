3a2ae2fb665f46d13cf6ce7fe23c54ab16b8a4e4
MoonSanghee <psycho13th@gmail.com>
2024-08-27 13:10:26 +0900

    백준 2225 문제풀이 수정

--- Baekjoon/새 폴더/3_gold/2225.py
+++ Baekjoon/새 폴더/3_gold/2225.py
@@ -4,7 +4,7 @@ dp = [[0 for _ in range(k + 1)] for _ in range(n + 1)]
 dp[0][0] = 1
 
 for i in range(n + 1):
-    for j in range(k + 1):
+    for j in range(1, k + 1):
         dp[i][j] = dp[i - 1][j] + dp[i][j - 1]
 
 print(dp[n][k] % 1000000000)
