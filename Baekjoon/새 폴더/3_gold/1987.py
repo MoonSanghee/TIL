508d016138c372377f3a895869ea048a9cbd2959
MoonSanghee <psycho13th@gmail.com>
2024-09-04 13:10:36 +0900

    백준 1987 문제 해설 추가

--- Baekjoon/새 폴더/3_gold/1987.py
+++ Baekjoon/새 폴더/3_gold/1987.py
@@ -1,19 +1,17 @@
 r, c = map(int, input().split())
-seet = []
-for i in range(r):
-    line = list(input())
-    seet.append(line)
-
+seet = [list(input()) for _ in range(r)]
+# 주어지는 입력의 크기를 받고 입력을 받아줍니다.
 result = 0
 alpha = set()
 alpha.add(seet[0][0])
-
+# 최장 길이를 담을 변수를 설정하고 사용한 알파벳을 담을 세트를 만든뒤 시작점을 넣어줍니다.
 dx = [1, -1, 0, 0]
 dy = [0, 0, 1, -1]
-
+# 4방향 탐색을 설정해줍니다.
 def dfs(x, y, z):
     global result
     result = max(result, z)
+    # 함수가 실행될때 최장길이를 확인하고 갱신해줍니다
     for i in range(4):
         nx = x + dx[i]
         ny = y + dy[i]
@@ -21,7 +19,9 @@ def dfs(x, y, z):
             alpha.add(seet[nx][ny])
             dfs(nx, ny, z + 1)
             alpha.remove(seet[nx][ny])
+        # 4방향 탐색을 통해 사용하지 않은 칸이라면 방문해줍니다. 
     return
 
 dfs(0, 0, 1)
 print(result)
+# 함수를 실행하고 결과를 출력해줍니다.
