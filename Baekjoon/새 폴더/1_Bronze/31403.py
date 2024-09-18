a3c0c8d2d377d8408a92f199b9f10512a940d280
MoonSanghee <psycho13th@gmail.com>
2024-09-18 18:53:06 +0900

    백준 31403 문제풀이 추가

---
+++ Baekjoon/새 폴더/1_Bronze/31403.py
@@ -0,0 +1,5 @@
+numbers = [input() for _ in range(3)]
+# 숫자를 받아줍니다.
+print(int(numbers[0]) + int(numbers[1]) - int(numbers[2]))
+print(int(numbers[0] + numbers[1]) - int(numbers[2]))
+# 주어진 방식에 맏게 결과를 출력해줍니다.
