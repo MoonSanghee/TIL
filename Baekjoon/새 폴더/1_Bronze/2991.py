8d01624a121729effa9c063d35a49d3c6c681ddc
MoonSanghee <psycho13th@gmail.com>
2025-02-09 19:48:26 +0900

    백준 1952 문제풀이 추가

--- Baekjoon/새 폴더/1_Bronze/2991.py
+++ Baekjoon/새 폴더/1_Bronze/2991.py
@@ -3,9 +3,9 @@ deliveries = list(map(int, input().split()))
 # 개가 활동하는 시간과 배달이 오는 시간을 받아줍니다
 for i in deliveries:
     result = 0
-    if 0 <= i % (a + b) <= a:
+    if 0 < i % (a + b) <= a:
         result += 1
-    if 0 <= i % (c + d) <= c:
+    if 0 < i % (c + d) <= c:
         result += 1
     # 각 배달이 올 때 두 개가 각각 활동중인지 확인해줍니다
     print(result)
