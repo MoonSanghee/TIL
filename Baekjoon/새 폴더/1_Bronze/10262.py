25382f61b362250835e77c4452654571bd650c2e
MoonSanghee <psycho13th@gmail.com>
2025-02-27 15:56:29 +0900

    백준 10262 문제풀이 추가

---
+++ Baekjoon/새 폴더/1_Bronze/10262.py
@@ -0,0 +1,6 @@
+a = sum(list(map(int, input().split())))
+b = sum(list(map(int, input().split())))
+if a>b:print("Emma")
+elif a==b:print("Tie")
+else:print("Gunnar")
+# 두 사람이 가진 주사위 눈의 합을 비교하여 높은 쪽을 출력해줍니다
