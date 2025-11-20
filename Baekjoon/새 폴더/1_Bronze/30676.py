ca4b494f7fd15fbd31a9182b37a4e13f59ca1c9a
MoonSanghee <psycho13th@gmail.com>
2025-11-20 16:52:38 +0900

    백준 30676 문제풀이 추가

---
+++ Baekjoon/새 폴더/1_Bronze/30676.py
@@ -0,0 +1,17 @@
+n = int(input())
+
+if 620 <= n <= 780:
+    print("Red")
+elif 590 <= n:
+    print("Orange")
+elif 570 <= n:
+    print("Yellow")
+elif 495 <= n:
+    print("Green")
+elif 450 <= n:
+    print("Blue")
+elif 425 <= n:
+    print("Indigo")
+elif 380 <= n:
+    print("Violet")
+# 주어진 파장의 색을 출력해줍니다
