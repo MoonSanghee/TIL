8ed441aaccaf4978c72a22863213d71b6a515227
MoonSanghee <psycho13th@gmail.com>
2026-05-11 22:04:36 +0900

    SWEA 13218 문제풀이 추가

---
+++ SWEA/D3/13218.py
@@ -0,0 +1,6 @@
+TC = int(input())
+# 테스트케이스의 개수를 받아줍니다
+for T in range(TC):
+    n = int(input())
+    print(f'#{T + 1} {n // 3}')
+    # 주어지는 정수를 받아 3으로 나눈 몫을 출력해줍니다
--- SWEA/D3/4406.py
+++ SWEA/D3/4406.py
@@ -1,11 +1,12 @@
 tc = int(input())
-
+# 테스트케이스가 몇개 주어지는지 받아줍니다
 for t in range(tc):
     word = input()
     result = ''
-
+    # 단어를 받고 결과를 담을 변수를 설정해줍니다 
     for i in word:
         if i not in 'aeiou':
             result += i
-    
+    # 모음이 아니라면 결과에 추가해줍니다
     print(f'#{t + 1} {result}')
+    # 주어진 양식에 맞게 결과를 출력해줍니다
